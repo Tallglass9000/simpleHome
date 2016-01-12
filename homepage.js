8710a9d0f23bb9d320c8d25d91095b8dbe404f47
Lady Blue <lady.blue2015@yahoo.com>
2016-01-11 16:42:38 -0800

    first commit

---
+++ homepage.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+	// This code comes from http://syddev.com/jquery.videoBG/
+	$('body').videoBG({
+		position:"fixed",
+		zIndex:-1,
+		mp4:'pinkblossoms_videoblocks.mp4'
+	});
+});
